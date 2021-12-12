640ec6a679f0c58dac9e720067342936ba3e9f1f
frederikhors <41120635+frederikhors@users.noreply.github.com>
2021-12-12 12:30:22 +0100

    remove redundant alias

--- client_test.go
+++ client_test.go
@@ -15,7 +15,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	backoff "gopkg.in/cenkalti/backoff.v1"
+	"gopkg.in/cenkalti/backoff.v1"
 )
 
 var url string
