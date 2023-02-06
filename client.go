031431a7e8e1cde094160fedca25365832a828da
Patrick Dawkins <patrick@platform.sh>
2023-02-06 23:46:59 +0000

    client: remove EOF special case

--- client.go
+++ client.go
@@ -10,7 +10,6 @@ import (
 	"encoding/base64"
 	"errors"
 	"fmt"
-	"io"
 	"net/http"
 	"sync"
 	"sync/atomic"
@@ -213,10 +212,6 @@ func (c *Client) readLoop(reader *EventStreamReader, outCh chan *Event, erChan c
 		// Read each new line and process the type of event
 		event, err := reader.ReadEvent()
 		if err != nil {
-			if err == io.EOF {
-				erChan <- nil
-				return
-			}
 			// run user specified disconnect function
 			if c.disconnectcb != nil {
 				c.Connected = false
