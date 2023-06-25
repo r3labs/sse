1b081717f1f52ada5f82a84dca7579d30e2d2afa
wuyi <wuyi@xiaoduotech.com>
2023-06-25 16:04:00 +0800

    support custom split func

--- event.go
+++ event.go
@@ -32,30 +32,41 @@ type EventStreamReader struct {
 }
 
 // NewEventStreamReader creates an instance of EventStreamReader.
-func NewEventStreamReader(eventStream io.Reader, maxBufferSize int) *EventStreamReader {
+func NewEventStreamReader(eventStream io.Reader, maxBufferSize int, split bufio.SplitFunc) *EventStreamReader {
 	scanner := bufio.NewScanner(eventStream)
 	initBufferSize := minPosInt(4096, maxBufferSize)
 	scanner.Buffer(make([]byte, initBufferSize), maxBufferSize)
 
-	split := func(data []byte, atEOF bool) (int, []byte, error) {
-		if atEOF && len(data) == 0 {
+	if split == nil {
+		split = func(data []byte, atEOF bool) (int, []byte, error) {
+			if atEOF && len(data) == 0 {
+				return 0, nil, nil
+			}
+
+			// We have a full event payload to parse.
+			if i, nlen := containsDoubleNewline(data); i >= 0 {
+				return i + nlen, data[0:i], nil
+			}
+			// If we're at EOF, we have all of the data.
+			if atEOF {
+				return len(data), data, nil
+			}
+			// Request more data.
 			return 0, nil, nil
 		}
 
-		// We have a full event payload to parse.
-		if i, nlen := containsDoubleNewline(data); i >= 0 {
-			return i + nlen, data[0:i], nil
-		}
-		// If we're at EOF, we have all of the data.
-		if atEOF {
-			return len(data), data, nil
-		}
-		// Request more data.
-		return 0, nil, nil
 	}
 	// Set the split function for the scanning operation.
 	scanner.Split(split)
 
+	return &EventStreamReader{
+		scanner: scanner,
+	}
+}
+
+// NewEventStreamReaderFromScanner creates an instance of EventStreamReader from scanner.
+// can custom scanner's split and buffer size
+func NewEventStreamReaderFromScanner(scanner *bufio.Scanner) *EventStreamReader {
 	return &EventStreamReader{
 		scanner: scanner,
 	}
